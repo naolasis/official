97beb1f02848bdb53f17432a33de0a2e8b86b608
Naol Sisay <naolasis02@gmil.com>
2024-11-01 01:00:38 +0300

    Update my portfolio, add project, add social media links

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
 
-
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav--visible');
 })
-function getVal() {
-    const val = document.querySelector('input').value;
-    console.log(val);
+
+// Listen for scroll event
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    const upArrow = document.getElementById("upArrow");
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+      upArrow.style.display = "block"; // Show the arrow
+    } else {
+      upArrow.style.display = "none"; // Hide the arrow
     }
-console.log("tole")    
+  }
+  
+  // Scroll to top when arrow is clicked
+  function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+  
+const width = window.innerWidth;
+const footer = document.querySelector(".footer");
+const parent = document.querySelector(".contact-left");
+const parent2 = document.querySelector(".contact-right");
+if(width > 800){
+ parent.insertAdjacentElement('beforeend',footer);
+}
+
+window.addEventListener("resize", function() {
+  const newWidth = window.innerWidth;
+  if (newWidth > 800) {
+    parent.insertAdjacentElement('beforeend',footer);
+  } else if (newWidth < 800) {
+    parent2.insertAdjacentElement('afterend', footer);
+  }
+});
